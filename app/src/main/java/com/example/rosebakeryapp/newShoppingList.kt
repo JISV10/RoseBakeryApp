14f16c50e59dfaa1fc6e54d19fd8172df94d3596
Braulio Castelblanco C <77630984+brauCastelblanco@users.noreply.github.com>
2024-03-28 07:08:16 -0600

    Feature: MVP of new shopping list created

--- app/src/main/java/com/example/rosebakeryapp/newShoppingList.kt
+++ app/src/main/java/com/example/rosebakeryapp/newShoppingList.kt
@@ -7,15 +7,26 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,12 +35,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.rosebakeryapp.ui.theme.RoseBakeryAppTheme
 
-class NewShoppingList : ComponentActivity() {
+class NewShoppingListClass : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -39,7 +51,7 @@ class NewShoppingList : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ShoppingList()
+                    NewShoppingList()
                 }
             }
         }
@@ -47,47 +59,67 @@ class NewShoppingList : ComponentActivity() {
 }
 
 @Composable
-fun ShoppingList( modifier: Modifier = Modifier) {
+fun NewShoppingList( modifier: Modifier = Modifier) {
     val salmonPink = colorResource(id = R.color.salmon_pink)
-    Column (modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
+    var text by remember { mutableStateOf("")}
+    var quantity by remember { mutableStateOf("") }
+    val shoppingLista = remember { mutableStateListOf<Pair<String, String>>() }
+
+
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        TextField(
+            value = text,
+            onValueChange = {text = it},
+            label = { Text(text = "Item")},
+            modifier = Modifier.padding(8.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+        )
+        TextField(
+            value = quantity,
+            onValueChange = {quantity = it},
+            label = { Text(text = "Quantity")},
+            modifier = Modifier.padding(8.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
 
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+                      if(text.isNotEmpty() && quantity.isNotEmpty()){
+                          shoppingLista.add(Pair(text, quantity))
+                          text = ""
+                          quantity = ""
+                      }
+            },
             modifier = Modifier
-                .padding(30.dp)
-                .size(width = 250.dp, height = 80.dp),
+                .align(Alignment.CenterHorizontally)
+                .padding(8.dp)
+            ,
             colors = ButtonDefaults.buttonColors(containerColor = salmonPink)
-
-        ) {
-
-            Text(
-                stringResource(id = R.string.new_shopping_list),
-                style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold)
-            )
-        }
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .padding(35.dp)
-                .size(width = 250.dp, height = 80.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = salmonPink)
-
-        ) {
-
-            Text(
-                stringResource(id = R.string.create_shopping_list),
-                style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold)
-            )
+            ) {
+            Text(text = "Add Item")
         }
     }
+    LazyColumn{
+        items(shoppingLista) { item ->
+            Row(
+                modifier.padding(vertical = 6.dp)
+            ){
+                Text("${item.first} - ${item.second}",
+                    style = MaterialTheme.typography.bodyMedium )
+            }
+        }
+    }
+
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun NewShoppingListPreview() {
     RoseBakeryAppTheme {
-        ShoppingList()
+        NewShoppingList()
     }
 }
